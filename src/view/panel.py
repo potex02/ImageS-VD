02b7e3686bbad2fd0d097b80437fc36fd8f65941
potex02 <roccopiopz@gmail.com>
2024-04-03 10:35:13 +0200

    Added QSlider to Panel

--- src/view/panel.py
+++ src/view/panel.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QSlider
 
 
 class Panel(QWidget):
@@ -20,10 +20,15 @@ class Panel(QWidget):
         """
         super().__init__()
         self._image: QPixmap = QPixmap(path)
+        self._slider: QSlider = QSlider(Qt.Horizontal)
+        self._slider.setMinimum(0)
+        self._slider.setMaximum(100)
+        self._slider.valueChanged.connect(lambda: print(self._slider.value()))
         layout: QVBoxLayout = QVBoxLayout()
         layout.setAlignment(Qt.AlignHCenter)
         self._image = self._image.scaled(400, 400, Qt.KeepAspectRatioByExpanding)
         label: QLabel = QLabel()
         label.setPixmap(self._image)
+        layout.addWidget(self._slider)
         layout.addWidget(label)
         self.setLayout(layout)
