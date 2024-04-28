57a621ce7dccf8dc8c4135179511adda2a4a9f59
potex02 <roccopiopz@gmail.com>
2024-04-28 14:14:29 +0200

    Added tab closing using central mouse button

--- src/view/tab_widget.py
+++ src/view/tab_widget.py
@@ -1,10 +1,17 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QTabWidget, QApplication
+from PySide6.QtCore import Qt, QCoreApplication
+from PySide6.QtGui import QMouseEvent
+from PySide6.QtWidgets import QTabWidget, QTabBar
 
 
 class TabWidget(QTabWidget):
     """
     The class representing the tabs of the application.
+
+    Methods:
+        mouse_event(self, event: QMouseEvent) -> None:
+            Handles the mouse press on the tab title.
+        close_tab(index: int) -> None:
+            Closes a tab of the widget and closes the app if there isn't any open tab.
     """
 
     def __init__(self) -> None:
@@ -12,5 +19,32 @@ class TabWidget(QTabWidget):
         Creates a TabWidget.
         """
         super().__init__()
+        self.setTabBar(QTabBar(self))
+        self.tabBar().mousePressEvent = self.mouse_event
         self.setTabsClosable(True)
-        self.tabCloseRequested.connect(self.removeTab)
+        self.tabCloseRequested.connect(self.close_tab)
+
+    def close_tab(self, index: int) -> None:
+        """
+        Closes a tab of the widget and closes the app if there isn't any open tab.
+
+        Args:
+            index (int): The index of the tab to close.
+        """
+        self.removeTab(index)
+        if self.count() == 0:
+            QCoreApplication.quit()
+
+    def mouse_event(self, event: QMouseEvent) -> None:
+        """
+        Handles the mouse press on the tab title.
+
+        Args:
+            event (QMouseEvent): The event to handle.
+        """
+        if event.button() == Qt.MiddleButton:
+            index = self.tabBar().tabAt(event.pos())
+            if index != -1:
+                self.tabCloseRequested.emit(index)
+        else:
+            QTabBar.mousePressEvent(self.tabBar(), event)
